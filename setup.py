f560326abdba880c0866976a9ef589101e36366c
jankenshow <perchon001@gmail.com>
2025-05-22 09:25:11 +0900

    add python setup tools

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+import platform
+import subprocess
+import sys
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=""):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = [
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={extdir}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            "-DCMAKE_BUILD_TYPE=Release",
+        ]
+
+        if platform.system() == "Windows":
+            cmake_args += ["-G", "Ninja"]
+
+        build_temp = os.path.join(self.build_temp, ext.name)
+        if not os.path.exists(build_temp):
+            os.makedirs(build_temp)
+
+        subprocess.check_call(["cmake", ext.sourcedir] + cmake_args, cwd=build_temp)
+        subprocess.check_call(["cmake", "--build", ".", "--config", "Release"], cwd=build_temp)
+
+
+setup(
+    ext_modules=[CMakeExtension("rolling_guidance_filter")],
+    cmdclass=dict(build_ext=CMakeBuild),
+)
