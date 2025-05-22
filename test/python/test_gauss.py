ac5701acd129021e3ad10fbb9e61061cb3d2c955
jankenshow <perchon001@gmail.com>
2025-05-22 20:46:18 +0900

    fix pytest problems

--- test/python/test_gauss.py
+++ test/python/test_gauss.py
@@ -1,9 +1,9 @@
 import os
-import sys
 import time
 
+# import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(os.path.join(BASE_DIR, "build"))
+# sys.path.append(os.path.join(BASE_DIR, "build"))
 
 import cv2
 import numpy as np
@@ -11,34 +11,34 @@ import numpy as np
 # import pytest
 
 try:
-    import rgf_pybind
+    import rgf
 except ImportError:
-    rgf_pybind = None
+    rgf = None
 
 
 def test_rgf_identity():
-    if rgf_pybind is None:
+    if rgf is None:
         return
     img = cv2.imread(os.path.join(BASE_DIR, "test/data/dog.jpg"))
-    out = rgf_pybind.gaussian_blur(img, sigma=3.0)
+    out = rgf.gaussian_blur(img, sigma=3.0)
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/dog_gauss.jpg"), out.astype(np.uint8))
 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imwrite(os.path.join(BASE_DIR, "test/data/dog_gray.jpg"), gray)
-    out_gray = rgf_pybind.gaussian_blur(gray, sigma=3.0)
+    out_gray = rgf.gaussian_blur(gray, sigma=3.0)
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/dog_gray_gauss.jpg"), out_gray.astype(np.uint8))
 
     img = cv2.imread(os.path.join(BASE_DIR, "test/data/image.png"))
     start_time = time.time()
-    out = rgf_pybind.gaussian_blur(img, sigma=3.0)
+    out = rgf.gaussian_blur(img, sigma=3.0)
     end_time = time.time()
     print(f"Time taken: {end_time - start_time} seconds")
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/image_gauss.png"), out.astype(np.uint8))
 
 
 if __name__ == "__main__":
-    if rgf_pybind is None:
-        print("rgf_pybind not built")
+    if rgf is None:
+        print("rgf not built")
     else:
         test_rgf_identity()
         print("Test passed.")
--- test/python/test_rgf.py
+++ test/python/test_rgf.py
@@ -1,9 +1,9 @@
 import os
-import sys
 import time
 
+# import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-sys.path.append(os.path.join(BASE_DIR, "build"))
+# sys.path.append(os.path.join(BASE_DIR, "build"))
 
 import cv2
 import numpy as np
@@ -11,33 +11,33 @@ import numpy as np
 # import pytest
 
 try:
-    import rgf_pybind
+    import rgf
 except ImportError:
-    rgf_pybind = None
+    rgf = None
 
 
 def test_rgf_identity():
-    if rgf_pybind is None:
+    if rgf is None:
         return
     img = cv2.imread(os.path.join(BASE_DIR, "test/data/dog.jpg"))
-    out = rgf_pybind.rolling_guidance_filter(img, sigma_s=3.0, sigma_r=10.0, iterations=3)
+    out = rgf.rolling_guidance_filter(img, sigma_s=3.0, sigma_r=10.0, iterations=3)
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/dog_rgf.jpg"), out.astype(np.uint8))
 
     gray = cv2.imread(os.path.join(BASE_DIR, "test/data/dog_gray.jpg"))
-    out_gray = rgf_pybind.rolling_guidance_filter(gray, sigma_s=3.0, sigma_r=10.0, iterations=3)
+    out_gray = rgf.rolling_guidance_filter(gray, sigma_s=3.0, sigma_r=10.0, iterations=3)
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/dog_gray_rgf.jpg"), out_gray.astype(np.uint8))
 
     img = cv2.imread(os.path.join(BASE_DIR, "test/data/image.png"))
     start_time = time.time()
-    out = rgf_pybind.rolling_guidance_filter(img, sigma_s=3.0, sigma_r=25.0, iterations=4)
+    out = rgf.rolling_guidance_filter(img, sigma_s=3.0, sigma_r=25.0, iterations=4)
     end_time = time.time()
     print(f"Time taken: {end_time - start_time} seconds")
     cv2.imwrite(os.path.join(BASE_DIR, "test/out/image_rgf.png"), out.astype(np.uint8))
 
 
 if __name__ == "__main__":
-    if rgf_pybind is None:
-        print("rgf_pybind not built")
+    if rgf is None:
+        print("rgf not built")
     else:
         test_rgf_identity()
         print("Test passed.")
